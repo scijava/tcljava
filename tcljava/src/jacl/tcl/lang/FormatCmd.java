2a5cf90878aa27bf43647613a97eb71857f5c676
hylands <hylands>
1999-03-22 07:37:41 +0000

    'java::field java.lang.Double NaN' was printing out a bogus value, it should print out NaN

--- tcljava/src/jacl/tcl/lang/FormatCmd.java
+++ tcljava/src/jacl/tcl/lang/FormatCmd.java
@@ -7,7 +7,7 @@
  * redistribution of this file, and for a DISCLAIMER OF ALL
  * WARRANTIES.
  * 
- * RCS: @(#) $Id: FormatCmd.java,v 1.1 1998/10/14 21:09:19 cvsadmin Exp $
+ * RCS: @(#) $Id: FormatCmd.java,v 1.2 1999/03/22 07:37:41 hylands Exp $
  *
  */
 
@@ -683,6 +683,9 @@ class FormatCmd implements Command {
 	 */
 
 	exp = 0;
+ 	if ((new Double(dblValue)).isNaN()) {
+            return "NaN";
+        }
 	if (dblValue>0.0) {
 	    int k = 0;
 	    while ((dblValue >= 1e8) && (k++ < 100)) {
