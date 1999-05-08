86b2159ef25bced7994a96b45cea25a97698c548
dejong <dejong>
1999-05-08 06:02:36 +0000

    moved to src/Test.java for 1.2.1

--- tcljava/unix/Test.java
+++
@@ -1,8 +0,0 @@
-//This class is used as a simple test for the compiler and runtime
-
-public class Test {
-  public static void main(String[] argv) {
-    //do nothing just exit with a valid result
-    System.exit(0);
-  }
-}
