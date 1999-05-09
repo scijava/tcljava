369ae30fbe17ebfd42a63978fd73fb0ff6ec2b43
dejong <dejong>
1999-05-09 01:30:54 +0000

    fixed comments

--- tcljava/src/jacl/tcl/lang/StrtoulResult.java
+++ tcljava/src/jacl/tcl/lang/StrtoulResult.java
@@ -10,36 +10,30 @@
  * redistribution of this file, and for a DISCLAIMER OF ALL
  * WARRANTIES.
  * 
- * RCS: @(#) $Id: StrtoulResult.java,v 1.1 1998/10/14 21:09:19 cvsadmin Exp $
+ * RCS: @(#) $Id: StrtoulResult.java,v 1.2 1999/05/09 01:30:54 dejong Exp $
  *
  */
 
 package tcl.lang;
 
-/*
+/**
  * This class stores the result of the Util.strtoul() method.
  */
 
 class StrtoulResult {
 
-/*
- * If the conversion is successful, errno = 0;
- *
- * If the number cannot be converted to a valid unsigned 32-bit integer,
- * contains the error code (TCL.INTEGER_RANGE or TCL.UNVALID_INTEGER).
- */
+// If the conversion is successful, errno = 0;
+//
+// If the number cannot be converted to a valid unsigned 32-bit integer,
+// contains the error code (TCL.INTEGER_RANGE or TCL.INVALID_INTEGER).
 
 int errno;
 
-/*
- * If errno is 0, points to the character right after the number
- */
+// If errno is 0, points to the character right after the number
 
 int index;
 
-/*
- * If errno is 0, contains the value of the number.
- */
+// If errno is 0, contains the value of the number.
 
 long value;
 
