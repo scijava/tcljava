4b211cd368af4921d6aaddb0b990e0a59958b833
dejong <dejong>
1999-05-10 03:43:22 +0000

    no longer needed after decompiling .class files

--- tcljava/src/jacl/tcl/regex/Placeholder.java
+++
@@ -1,8 +0,0 @@
-// this class is only used to make the compiler happy when
-// no sources are provided in regex, it is also used to
-// make the build rule happy is case there are no compiled
-// regexp classes in the jacl/tcl/regex directory
-package tcl.regex;
-class Placeholder {
-	private Placeholder() {}
-}
