2071ac81f22812c1669df1fcabdb60c6ac874b0f
dejong <dejong>
1999-05-11 04:05:44 +0000

    changed tmp var names to avoid collision

--- tcljava/win/jaclsh.bat
+++ tcljava/win/jaclsh.bat
@@ -1,14 +1,12 @@
 @echo off
-rem this bat file is used to start jacl
+rem this bat file is used to start jacl, it shoud be in your PATH
 rem Copyright (c) 1997-1998 by Moses DeJong
+rem See license.terms file for copyright info
 
-set PREFIX=G:\mo\software\test_tcl
+set __PREFIX=G:\mo\software\test_tcl
 
-set JAVA=G:\mo\software\java\jdk1.1.6\bin\java
+set __JAVA_PREFIX=G:\mo\software\java\jdk1.1.7
+set __JAVA_EXECUTABLE=%__JAVA_PREFIX%\bin\java
+set __JAVA_CLASSPATH=%__JAVA_PREFIX%\lib\classes.zip;%__JAVA_PREFIX%\lib\jacl.jar;%__JAVA_PREFIX%\lib\tcljava.jar
 
-rem Some JDK's might want -native in the line below
-set JAVA_FLAGS="-ms5m -mx20m"
-
-set CLASSPATH=%PREFIX%\lib\jacl.jar;%PREFIX%\lib\tcljava.jar;%CLASSPATH%
-
-%JAVA% %JAVA_FLAGS% tcl.lang.Shell %1 %2 %3 %4 %5 %6 %7 %8 %9
+%JAVA_EXECUTABLE% -ms5m -mx20m -classpath %__JAVA_CLASSPATH% tcl.lang.Shell %1 %2 %3 %4 %5 %6 %7 %8 %9
