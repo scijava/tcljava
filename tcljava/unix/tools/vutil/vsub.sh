f9de9eb52bfb86b61e55f30f4abb477597d49f50
dejong <dejong>
1999-05-17 03:58:06 +0000

    use version number from configure.in instead of RCS version

--- tcljava/unix/tools/vutil/vsub.sh
+++ tcljava/unix/tools/vutil/vsub.sh
@@ -5,32 +5,14 @@ CUR=`pwd`/current
 RCSVER=`pwd`/rcsver.tcl
 FSUB=`pwd`/fsub.tcl
 
+
 # go to the root dir in the dist
 cd ../../..
 
 
-# double check that this is the correct directory
+# get first argument passed into this shell
 
-if test ! -f changes.txt; then
-    echo "`pwd`/changes.txt does not exist!, please check vsub.sh"
-    exit -1
-fi
-
-
-TCLJAVA_VERSION=`$TCLSH $RCSVER changes.txt`
-#TCLJAVA_VERSION=1.2.1
-
-# Find out if we need to update the version numbers by checking to
-# see if the one we got differs from the one in the file current.
-
-if env grep "TCLJAVA_VERSION=$TCLJAVA_VERSION" $CUR > /dev/null; then
-    # If they are the same we do not need to update
-    exit 0
-else
-    # Otherwise we do need to update
-    echo "Updating version numbers in the source files to $TCLJAVA_VERSION"
-fi
-
+TCLJAVA_VERSION=${1}
 
 
 # Update version numbers of the files with package statements in them
@@ -45,8 +27,9 @@ $TCLSH $FSUB '(package +[a-z]* +java +)[0-9]+(\.[0-9]+)*' \
 
 # Update version numbers of the files with TCLJAVA_VERSION in them
 
-FILES="unix/configure.in"
-FILES="$FILES unix/configure"
+FILES=""
+#FILES="$FILES unix/configure.in"
+#FILES="$FILES unix/configure"
 FILES="$FILES win/makefile.vc"
 FILES="$FILES $CUR"
 
