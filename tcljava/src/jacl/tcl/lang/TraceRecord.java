db839a3fd197930fe8c2dbddb7e137deabe89439
mo <mo>
1999-07-28 03:27:36 +0000

    fixed comments

--- tcljava/src/jacl/tcl/lang/TraceRecord.java
+++ tcljava/src/jacl/tcl/lang/TraceRecord.java
@@ -10,20 +10,20 @@
  * redistribution of this file, and for a DISCLAIMER OF ALL
  * WARRANTIES.
  * 
- * RCS: @(#) $Id: TraceRecord.java,v 1.1 1998/10/14 21:09:18 cvsadmin Exp $
+ * RCS: @(#) $Id: TraceRecord.java,v 1.2 1999/07/28 03:27:36 mo Exp $
  *
  */
 
 package tcl.lang;
 
-/*
+/**
  * This class is used internally by CallFrame to store one variable
  * trace.
  */
 
 class TraceRecord {
 
-/*
+/**
  * Stores info about the conditions under which this trace should be
  * triggered. Should be a combination of TCL.TRACE_READS, TCL.TRACE_WRITES
  * or TCL.TRACE_UNSETS.
@@ -31,7 +31,7 @@ class TraceRecord {
 
 int flags;
 
-/*
+/**
  * Stores the trace procedure to invoke when a trace is fired.
  */
 
