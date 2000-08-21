1f2d35f19e15d2fff11065cac5bd243fd8a70c9a
mo <mo>
2000-08-21 04:48:11 +0000

    Update lreplace command to be 8.4 compliant

--- tcljava/src/jacl/tcl/lang/LreplaceCmd.java
+++ tcljava/src/jacl/tcl/lang/LreplaceCmd.java
@@ -8,7 +8,7 @@
  * redistribution of this file, and for a DISCLAIMER OF ALL
  * WARRANTIES.
  * 
- * RCS: @(#) $Id: LreplaceCmd.java,v 1.3 2000/03/17 23:31:30 mo Exp $
+ * RCS: @(#) $Id: LreplaceCmd.java,v 1.4 2000/08/21 04:48:11 mo Exp $
  *
  */
 
@@ -25,35 +25,44 @@ class LreplaceCmd implements Command {
      */
 
     public void cmdProc(Interp interp, TclObject argv[])
-	    throws TclException {
-	if (argv.length < 4) {
-	    throw new TclNumArgsException(interp, 1, argv, 
+            throws TclException {
+        if (argv.length < 4) {
+            throw new TclNumArgsException(interp, 1, argv, 
                     "list first last ?element element ...?");
         }
-	int size = TclList.getLength(interp, argv[1]);
-	int first;
-	int last;
+        int size  = TclList.getLength(interp, argv[1]);
+        int first = Util.getIntForIndex(interp, argv[2], size-1);
+        int last  = Util.getIntForIndex(interp, argv[3], size-1);
+        int numToDelete;
 
-	first = Util.getIntForIndex(interp, argv[2], size-1);
-	last  = Util.getIntForIndex(interp, argv[3], size-1);
+        if (first < 0) {
+            first = 0;
+        }
 
-	if (first < 0) {
-	    first = 0;
-	}
-	if (first >= size) {
-	    throw new TclException(interp, "list doesn't contain element " +
-		    argv[2]);
-	}
-	if (last >= size) {
-	    last = size - 1;
-	}
+        // Complain if the user asked for a start element that is greater
+        // than the list length. This won't ever trigger for the "end*"
+        // case as that will be properly constrained by getIntForIndex
+        // because we use size-1 (to allow for replacing the last elem).
+
+        if ((first >= size) && (size > 0)) {
+            throw new TclException(interp, "list doesn't contain element " +
+                argv[2]);
+        }
+        if (last >= size) {
+            last = size - 1;
+        }
+        if (first <= last) {
+            numToDelete = (last - first + 1);
+        } else {
+            numToDelete = 0;
+        }
 
 	TclObject list = argv[1];
 	list.preserve();
 	list = list.takeExclusive();
 
 	try {
-	    TclList.replace(interp, list, first, last-first+1, argv, 4,
+	    TclList.replace(interp, list, first, numToDelete, argv, 4,
 		    argv.length-1);
 	    interp.setResult(list);
 	} finally {
