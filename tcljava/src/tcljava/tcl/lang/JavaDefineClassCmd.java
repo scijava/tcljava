d7581b18977ba35cbf73ad4ed006f5dc2cf859a0
mdejong <mdejong>
2003-03-19 02:35:57 +0000

    * src/tcljava/tcl/lang/JavaDefineClassCmd.java (cmdProc): 	Convert each char to a byte instead of calling 	String.getBytes() which returns bytes in the 	platform's default encoding.

--- tcljava/src/tcljava/tcl/lang/JavaDefineClassCmd.java
+++ tcljava/src/tcljava/tcl/lang/JavaDefineClassCmd.java
@@ -8,7 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: JavaDefineClassCmd.java,v 1.2 2002/12/30 02:30:54 mdejong Exp $
+ * RCS: @(#) $Id: JavaDefineClassCmd.java,v 1.3 2003/03/19 02:35:57 mdejong Exp $
  */
 
 package tcl.lang;
@@ -43,17 +43,26 @@ throws
     byte[] classData;
     Class  result;
     TclClassLoader tclClassLoader;
-    
+
     if (argv.length != 2) {
 	throw new TclNumArgsException(interp, 1, argv,
 		"classbytes");
     }
 
-    classData = argv[1].toString().getBytes();
+    // FIXME: It would be better if the TclByteArray class
+    // was available in both Tcl Blend and Jacl so that we
+    // could query bytes directly instead of converting to
+    // a string and then converting back to bytes.
+    String str = argv[1].toString();
+    final int str_length = str.length();
+    classData = new byte[str_length];
+    for (int i=0; i < str_length; i++) {
+        classData[i] = (byte) str.charAt(i);
+    }
 
     tclClassLoader = new TclClassLoader(interp, null);
     result = tclClassLoader.defineClass(null, classData);
-    
+
     interp.setResult(ReflectObject.newInstance(interp,
             Class.class, result));
 }
