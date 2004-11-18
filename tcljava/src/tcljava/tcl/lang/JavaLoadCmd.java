a6a2904e8cd731d378bf4bed5ec283df55aece72
mdejong <mdejong>
2004-11-18 06:40:17 +0000

    * Makefile.in: 	* src/tcljava/tcl/lang/JavaLoadCmd.java: 	* src/tests/tcljava/javaload/Test11Extension.java: 	* tests/tcljava/JavaLoadCmd.test: 	Add check for class dependency errors while 	creating an instance of an Extension object 	via the java::load command. 	[Patch 1016586]

--- tcljava/src/tcljava/tcl/lang/JavaLoadCmd.java
+++ tcljava/src/tcljava/tcl/lang/JavaLoadCmd.java
@@ -8,7 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: JavaLoadCmd.java,v 1.2 1999/05/09 22:17:14 dejong Exp $
+ * RCS: @(#) $Id: JavaLoadCmd.java,v 1.3 2004/11/18 06:40:17 mdejong Exp $
  */
 
 package tcl.lang;
@@ -141,6 +141,14 @@ throws
     } catch (Exception e) {
 	throw new TclException (interp, errorMsg +
 		"can't find class \"" + e.getMessage() + "\"");
+    } catch (LinkageError e) {
+        // Known to covers these error conditions:
+        // NoClassDefFoundError
+        // ExceptionInInitializerError
+        throw new TclException(interp, "Extension \"" +
+		packageName +
+		"\" contains a dependency \"" + e.getMessage() +
+		"\" that could not be resolved.");
     } finally {
 	// If we did not have a valid load, the packageName
 	// must be removed from the static tclClassLoader's 
