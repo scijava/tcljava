4f55b75c6ce0cebd3f6c64cf6e4269cb4ff84b82
mdejong <mdejong>
2006-01-31 00:49:21 +0000

    * docs/TclJava/JavaDefineClassCmd.html:         * src/tcljava/tcl/lang/JavaDefineClassCmd.java:         * src/tcljava/tcl/lang/TclClassLoader.java:         * src/tests/tcljava/javaload/Test3Cmd.java:         * src/tests/tcljava/javaload/Test4Cmd.java:         * tests/tcljava/JavaDefineClassCmd.test:         Add ability to directly pass byte[] object         to the java::defineclass command.

--- tcljava/src/tcljava/tcl/lang/JavaDefineClassCmd.java
+++ tcljava/src/tcljava/tcl/lang/JavaDefineClassCmd.java
@@ -8,7 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: JavaDefineClassCmd.java,v 1.3 2003/03/19 02:35:57 mdejong Exp $
+ * RCS: @(#) $Id: JavaDefineClassCmd.java,v 1.4 2006/01/31 00:49:21 mdejong Exp $
  */
 
 package tcl.lang;
@@ -40,7 +40,7 @@ cmdProc(
 throws
     TclException			// A standard Tcl exception.
 {
-    byte[] classData;
+    byte[] classData = null;
     Class  result;
     TclClassLoader tclClassLoader;
 
@@ -49,15 +49,33 @@ throws
 		"classbytes");
     }
 
-    // FIXME: It would be better if the TclByteArray class
-    // was available in both Tcl Blend and Jacl so that we
-    // could query bytes directly instead of converting to
-    // a string and then converting back to bytes.
-    String str = argv[1].toString();
-    final int str_length = str.length();
-    classData = new byte[str_length];
-    for (int i=0; i < str_length; i++) {
-        classData[i] = (byte) str.charAt(i);
+    TclObject classBytesObj = argv[1];
+
+    // If the classbytes argument is a ReflectObject
+    // that contains a byte[] then unwrap it and
+    // use the bytes directly. Creating a TclByteArray
+    // actually creates a copy of the passed in array,
+    // so passing the byte[] directly is faster.
+
+    if (classBytesObj.getInternalRep() instanceof ReflectObject) {
+        Object obj = ReflectObject.get(interp, classBytesObj);
+        if (obj instanceof byte[]) {
+            classData = (byte[]) obj;
+        }
+    }
+
+    if (classData == null) {
+        // FIXME: It would be better if the TclByteArray class
+        // was available in both Tcl Blend and Jacl so that we
+        // could query bytes directly instead of converting to
+        // a string and then converting back to bytes.
+
+        String str = classBytesObj.toString();
+        final int str_length = str.length();
+        classData = new byte[str_length];
+        for (int i=0; i < str_length; i++) {
+            classData[i] = (byte) str.charAt(i);
+        }
     }
 
     tclClassLoader = new TclClassLoader(interp, null);
