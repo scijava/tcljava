1571b19ff80d1f92c50ecde9e5f981e01c30f3a2
mdejong <mdejong>
2006-06-30 07:57:18 +0000

    * src/jacl/tcl/lang/ExecCmd.java: Remove scary         runtime exec logic and replace with         exec introduced in JDK 1.3. The old code is         no longer needed since we require JDK 1.4 now.

--- tcljava/src/jacl/tcl/lang/ExecCmd.java
+++ tcljava/src/jacl/tcl/lang/ExecCmd.java
@@ -10,16 +10,13 @@
  * redistribution of this file, and for a DISCLAIMER OF ALL
  * WARRANTIES.
  * 
- * RCS: @(#) $Id: ExecCmd.java,v 1.12 2006/04/05 23:03:38 mdejong Exp $
+ * RCS: @(#) $Id: ExecCmd.java,v 1.13 2006/06/30 07:57:18 mdejong Exp $
  */
 
 package tcl.lang;
 
 import java.util.*;
 import java.io.*;
-import java.lang.reflect.Method;
-import java.lang.reflect.InvocationTargetException;
-
 
 /*
  * This class implements the built-in "exec" command in Tcl.
@@ -27,29 +24,13 @@ import java.lang.reflect.InvocationTargetException;
 
 class ExecCmd implements Command {
 
-/**
- * Reference to Runtime.exec, null when JDK < 1.3
- */
-private static Method execMethod;
-
-static {
-    // Runtime.exec(String[] cmdArr, String[] envArr, File currDir)
-    Class[] parameterTypes = {String[].class, String[].class, File.class };
-    try {
-        execMethod = Runtime.getRuntime().getClass().getMethod("exec",
-            parameterTypes);
-    } catch (NoSuchMethodException e) {
-        execMethod = null;
-    }
-}
-
 private static boolean debug = false;
 
 
 /*
  *----------------------------------------------------------------------
  *
- * CmdProc --
+ * cmdProc --
  *
  *	This procedure is invoked to process the "exec" Tcl command.
  *	See the user documentation for details on what it does.
@@ -70,19 +51,17 @@ public void
 throws 
     TclException 	// A standard Tcl exception.
 {
-    int      firstWord;             /* Index to the first non-switch arg */
-    int      argLen = argv.length;  /* No of args to copy to argStrs     */
-    int      exit;                  /* denotes exit status of process    */
-    int      errorBytes;            /* number of bytes of process stderr */
-    boolean  background  = false;   /* Indicates a bg process            */
-    boolean  keepNewline = false;   /* Retains newline in pipline output */
-    Process  p;                     /* The exec-ed process               */
-    String   argStr;                /* Conversion of argv to a string    */
+    int      firstWord;             // Index to the first non-switch arg
+    int      argLen = argv.length;  // No of args to copy to argStrs
+    int      exit;                  // denotes exit status of process
+    int      errorBytes;            // number of bytes of process stderr
+    boolean  background  = false;   // Indicates a bg process
+    boolean  keepNewline = false;   // Retains newline in pipline output
+    Process  p;                     // The exec-ed process
+    String   argStr;                // Conversion of argv to a string
     StringBuffer sbuf;
 
-    /*
-     * Check for a leading "-keepnewline" argument.
-     */
+    // Check for a leading "-keepnewline" argument.
 
     for (firstWord = 1; firstWord < argLen; firstWord++) {
 	argStr = argv[firstWord].toString();
@@ -102,15 +81,12 @@ throws
     }
 
     if (argLen <= firstWord) {
-	    throw new TclNumArgsException(interp, 1, argv,
-		"?switches? arg ?arg ...?");
+	throw new TclNumArgsException(interp, 1, argv,
+	    "?switches? arg ?arg ...?");
     }
 
-
-    /*
-     * See if the command is to be run in background.
-     * Currently this does nothing, it is just for compatibility
-     */
+    // See if the command is to be run in background.
+    // Currently this does nothing, it is just for compatibility
 
     if (argv[argLen-1].toString().equals("&")) {
 	argLen--;
@@ -118,34 +94,7 @@ throws
     }
 
     try {
-	/*
-	 * It is necessary to perform system specific 
-	 * operations before calling exec.  For now Solaris
-	 * and Windows execs are somewhat supported, in all other cases
-	 * we simply call exec and give it our "best shot"
-	 */
-
-        if (execMethod != null) {
-	    if (debug) {
-	        System.out.println("using execReflection() for JDK 1.3 or newer");
-	    }
-	    p = execReflection(interp, argv, firstWord, argLen);
-        } else if (Util.isUnix()) {
-	    if (debug) {
-	        System.out.println("using execUnix() for old unix systems");
-	    }
-	    p = execUnix(interp, argv, firstWord, argLen);
-	} else if (Util.isWindows()) {
-	    if (debug) {
-	        System.out.println("using execWin() for old windows systems");
-	    }
-	    p = execWin(interp, argv, firstWord, argLen);
-	} else {
-	    if (debug) {
-	        System.out.println("using execDefault() for old unknown systems");
-	    }
-	    p = execDefault(interp, argv, firstWord, argLen);
-	}
+	p = execCmd(interp, argv, firstWord, argLen);
 
         // If user wanted to run in the background, then
         // don't wait for or attach to the stdout or stderr
@@ -157,76 +106,63 @@ throws
             return;
         }
 
-        // When running under a pre JDK 1.3 system, read data
-        // in the same thread. Some JDKs newer than 1.3 require
-        // that the data from a subprocess be read in another
-        // thread.
-
         sbuf = new StringBuffer();
 
-        if (execMethod != null) {
-            // JDK 1.3 or newer, so create a thread to read the
-            // stdout and stderr of the subprocess. Each stream
-            // needs to be handled by a serarate thread.
+        // Create a pair of threads to read stdout and stderr
+        // of the subprocess. Each stream needs to be handled
+        // by a serarate thread.
 
-            ExecInputStreamReader stdout_reader = new ExecInputStreamReader(
-                p.getInputStream());
+        ExecInputStreamReader stdout_reader = new ExecInputStreamReader(
+            p.getInputStream());
 
-            ExecInputStreamReader stderr_reader = new ExecInputStreamReader(
-                p.getErrorStream());
+        ExecInputStreamReader stderr_reader = new ExecInputStreamReader(
+            p.getErrorStream());
 
-            // Start reading threads, wait for process to terminate in
-            // this thread, then make sure other threads have terminated.
-            stdout_reader.start();
-            stderr_reader.start();
+        // Start reading threads, wait for process to terminate in
+        // this thread, then make sure other threads have terminated.
 
-            if (debug) {
-                System.out.println("started reader threads, invoking waitFor()");
-            }
+        stdout_reader.start();
+        stderr_reader.start();
 
-            exit = p.waitFor();
-
-            if (debug) {
-                System.out.println("waitFor() returned " + exit);
-                System.out.println("joining reader threads");
-            }
-
-            stdout_reader.join();
-            stderr_reader.join();
-
-            // Get stdout and stderr from other threads.
-
-            int numBytes;
-            numBytes = stdout_reader.appendBytes(sbuf);
-            if (debug) {
-                System.out.println("appended " + numBytes + " bytes from stdout stream");
-            }
-            numBytes = stderr_reader.appendBytes(sbuf);
-            if (debug) {
-                System.out.println("appended " + numBytes + " bytes from stderr stream");
-            }
-
-            errorBytes = stderr_reader.getInBytes();
-        } else {
-            // Earlier than JDK 1.3, read in same thread.
-            exit = p.waitFor();
-            readStreamIntoBuffer(p.getInputStream(), sbuf);
-
-            // Append any data from stderr stream to result
-            errorBytes = readStreamIntoBuffer(p.getErrorStream(), sbuf);
+        if (debug) {
+            System.out.println("started reader threads, invoking waitFor()");
         }
+
+        exit = p.waitFor();
+
+        if (debug) {
+            System.out.println("waitFor() returned " + exit);
+            System.out.println("joining reader threads");
+        }
+
+        stdout_reader.join();
+        stderr_reader.join();
+
+        // Get stdout and stderr from other threads.
+
+        int numBytes;
+        numBytes = stdout_reader.appendBytes(sbuf);
+        if (debug) {
+            System.out.println("appended " + numBytes + " bytes from stdout stream");
+        }
+        numBytes = stderr_reader.appendBytes(sbuf);
+        if (debug) {
+            System.out.println("appended " + numBytes + " bytes from stderr stream");
+        }
+
+        errorBytes = stderr_reader.getInBytes();
 
 	// Check for the special case where there is no error
 	// data but the process returns an error result
 
 	if ((errorBytes == 0) && (exit != 0)) {
-	  sbuf.append("child process exited abnormally");
+	    sbuf.append("child process exited abnormally");
 	}
 
-	// If the last character of the result buffer is a newline, then 
-	// remove the newline character (the newline would just confuse 
+	// If the last character of the result buffer is a newline, then
+	// remove the newline character (the newline would just confuse
 	// things).  Finally, we set pass the result to the interpreter.
-	
+
 	int length = sbuf.length();
 	if (!keepNewline && (length > 0) &&
 		(sbuf.charAt(length - 1) == '\n')) {
@@ -236,7 +172,7 @@ throws
 	// Tcl supports lots of child status conditions.
 	// Unfortunately, we can only find the child's
 	// exit status using the Java API
-        
+
 	if (exit != 0) {
 	    TclObject childstatus = TclList.newInstance();
 	    TclList.append(interp, childstatus,
@@ -252,25 +188,24 @@ throws
             interp.setErrorCode( childstatus );
 	}
 
-	//when the subprocess writes to its stderr stream or returns
-	//a non zero result we generate an error
+	// when the subprocess writes to its stderr stream or returns
+	// a non zero result we generate an error
+
 	if ((exit != 0) || (errorBytes != 0)) {
-	  throw new TclException(interp, sbuf.toString());
+	    throw new TclException(interp, sbuf.toString());
 	}
-	
-	//otherwise things went well so set the result
-	interp.setResult(sbuf.toString());
-	
+
+	// otherwise things went well so set the result
+
+	interp.setResult(sbuf.toString());	
     } catch (IOException e) {
-        //if exec fails we end up catching the exception here
+        // if exec fails we end up catching the exception here
 
 	throw new TclException(interp, "couldn't execute \"" +
 		argv[firstWord].toString() + "\": no such file or directory");
 
     } catch (InterruptedException e) {
-	/*
-	 * Do Nothing...
-	 */
+	// Do Nothing...
     }
 }
 
@@ -324,107 +259,15 @@ static int readStreamIntoBuffer(InputStream in, StringBuffer sbuf) {
     return numRead;
 }
 
-
 
 /*
  *----------------------------------------------------------------------
  *
- * escapeWinString --
+ * execCmd --
  *
- *	This utility function takes a String and returns the same String
- *      with any % replaced by %%. This is needed so that environemental
- *      variable substitution is not done when a windows program is called
- *      with the exec command that uses .bat files
- *
- * Results:
- *	Returns the same String is no % is in the string, otherwise the
- *      String is returned with %% for every %.
- *
- * Side effects:
- *	none.
- *
- *----------------------------------------------------------------------
- */
-
-static String escapeWinString(String str) {
-    if (str.indexOf('%') == -1)
-      return str;
-
-    char[] arr = str.toCharArray();
-    StringBuffer sb = new StringBuffer(50);
-
-    for (int i=0; i < arr.length; i++) {
-      if (arr[i] == '%') {
-        sb.append('%');
-      }
-      sb.append(arr[i]);
-    }
-
-    return sb.toString();
-}
-
-
-
-/*
- *----------------------------------------------------------------------
- *
- * execUnix --
- *
- *	This procedure is invoked to process system specific "exec" calls for
- *	Unix.  Since new Process dosent know the current tclsh directory, we
- *	exec the command in a Unix shell, but we force the Unix shell to cd to
- *	the current dir first. 
- *
- * Results:
- *	Returns the Process object returned from the exec call.
- *
- * Side effects:
- *	See the user documentation.
- *
- *----------------------------------------------------------------------
- */
-
-private Process 
-execUnix (Interp interp, TclObject argv[], int first, int last) 
-        throws IOException {
-    String[] argStrs = new String[3];
-
-    argStrs[0] = "sh";
-    argStrs[1] = "-c";
-
-    StringBuffer sbuf = new StringBuffer();
-
-    sbuf.append("cd \'");
-    sbuf.append(interp.getWorkingDir().toString());
-    sbuf.append("\'; ");
-    
-    for (int i = first; i < last; i++) {
-      sbuf.append('\'');
-      sbuf.append(argv[i].toString());
-      sbuf.append('\'');
-      sbuf.append(' ');
-    }
-
-    //trim off the last space char
-    sbuf.setLength(sbuf.length() - 1);
-
-    argStrs[2] = sbuf.toString();
-
-    //System.out.println("now to unix exec \"" + argStrs[2] + "\"");
-
-    return Runtime.getRuntime().exec(argStrs);
-}
-
-
-/*
- *----------------------------------------------------------------------
- *
- * execWin --
- *
- *	This procedure is invoked to process system specific "exec" calls for
- *	Windows.  Since new Process dosent know the current tclsh directory, a
- *	file has to be created that cds to the current interp dir, and executes
- *	the command there. 
+ *	This procedure is invoked to process the "exec" call assuming the
+ *	Runtime.exec( String[] cmdArr, String[] envArr, File currDir )
+ *      API exists (introduced in JDK 1.3).
  *
  * Results:
  *	Returns the new process.
@@ -435,201 +278,17 @@ execUnix (Interp interp, TclObject argv[], int first, int last)
  *----------------------------------------------------------------------
  */
 
-private Process 
- execWin (Interp interp, TclObject argv[], int first, int last) 
-    throws IOException {
-
-    // when running on NT we need to write out two files
-    // the first is in C:/TEMP/jacl1.bat and the second is C:/TEMP/jacl2.bat
-    // we exec command.com on jacl1.bat which will invoke cmd.exe on jacl2.bat
-
-    File tmp = new File("C:\\TEMP");
-    if (! tmp.exists()) {
-	if (! tmp.mkdirs()) {
-	    throw new IOException("could not create C:\\TEMP");
-	}
-    }
-    
-    // if we are not running under NT then we just write out the jacl1.bat file
-    // and invoke that with command.com which should work on 95
-
-    String jacl1 = "C:\\TEMP\\jacl1.bat";
-    String jacl2 = "C:\\TEMP\\jacl2.bat";
-    
-    String os_name = System.getProperty("os.name");
-    boolean isNT = os_name.equalsIgnoreCase("windows xp") ||
-                   os_name.equalsIgnoreCase("windows nt") ||
-                   os_name.equalsIgnoreCase("windows 2000");
-                   
-                   
-    
-    File jacl1_file = new File(jacl1);
-    File jacl2_file = new File(jacl2);
-
-
-    // If running on NT, write to jacl2.bat, otherwise write out to jacl1.bat.
-    // Also check that jacl1.bat exists before each exec in case it got
-    // deleted by the user.
-
-    File out_file;
-
-    if (isNT) {
-
-	if ( jacl1_file.exists() ) {
-	    jacl1_file.delete();
-	}
-	
-	PrintWriter jacl_out = new PrintWriter(new BufferedWriter(
-					       new FileWriter( jacl1_file )));
-	
-	jacl_out.println("@echo off");
-	jacl_out.println("cmd.exe /C " + jacl2);
-	jacl_out.close();
-	
-	out_file = jacl2_file;
-    } else {
-	out_file = jacl1_file;
-    }
-
-
-    if ( out_file.exists() ) {
-	out_file.delete();
-    }
-
-    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter( out_file )));
-
-
-    // now we write out the .BAT script to the current output file and run it
-
-    // remove .bat file cmd echoing
-    out.println("@echo off");
-
-    // from the path we get the "DRIVE" and the current directory
-    String path = interp.getWorkingDir().toString();
-    
-    // write out the drive id
-    out.println( path.substring(0,2) );
-
-    // write out the path without double quotes but with % subst
-    out.println( "cd " + escapeWinString(path.substring(2)) );
-
-
-    // we must take special care not to quote the program name
-    // we must also take care to ensure that each "%" becomes a "%%"
-    out.print( escapeWinString(argv[first].toString()) );
-    out.print(' ');
-
-
-    for (int i = (first + 1); i < last; i++) {
-      out.print('"');
-      out.print( escapeWinString(argv[i].toString()) );
-      out.print('"');
-      out.print(' ');
-    }
-
-    out.println();
-    out.close();
-
-    String[] argStrs = {"command.com", "/C", jacl1};
-
-    //System.out.println("now to win exec " + argv[first]);
-
-    return Runtime.getRuntime().exec(argStrs);
-}
-
-
-/*
- *----------------------------------------------------------------------
- *
- * execDefault --
- *
- *	This procedure is invoked to process the "exec" call for an unknown
- *	a system. This happens when we do not have special exec code for a
- *      system that the code is running under. It should never get used as
- *      unix and windows are supported and Mac has no exec.
- *
- * Results:
- *	Returns the new process.
- *
- * Side effects:
- *	See the user documentation.
- *
- *----------------------------------------------------------------------
- */
-
-private Process 
-execDefault (Interp interp, TclObject argv[], int first, int last) 
-        throws IOException {
-
-    //we can not do anything to change the current directory in Java
-
-
-    String[] strv = new String[last - first];
-
-    //System.out.println("def alloc space for " + (last - first) + " Strings");
-
-    for (int i=first, j=0; i < last; j++, i++) {
-        strv[j] = argv[i].toString();
-	//System.out.println("default exec arg = ->" + strv[j] + "<-");
-    }
-
-    return Runtime.getRuntime().exec(strv);
-}
-
-
-/*
- *----------------------------------------------------------------------
- *
- * execReflection --
- *
- *	This procedure is invoked to process the "exec" call using the
- *	new Runtime.exec( String[] cmdArr, String[] envArr, File currDir )
- *      call in JDK1.3. The method invocation is done using Reflection to
- *      allow for JDK1.1/1.2 compatibility.
- *
- * Results:
- *	Returns the new process.
- *
- * Side effects:
- *	See the user documentation.
- *
- *----------------------------------------------------------------------
- */
-
-private Process 
-execReflection (Interp interp, TclObject argv[], int first, int last) 
-        throws IOException {
-
+private Process
+execCmd(Interp interp, TclObject argv[], int first, int last) 
+        throws IOException
+{
     String[] strv = new String[last - first];
 
     for (int i=first, j=0; i < last; j++, i++) {
         strv[j] = argv[i].toString();
     }
 
-    Object[] methodArgs = new Object[3];
-    methodArgs[0] = strv; // exec command arguments
-    methodArgs[1] = null;  // inherit all environment variables
-    methodArgs[2] = interp.getWorkingDir();
-
-    try
-    {
-        return (Process)
-            execMethod.invoke(Runtime.getRuntime(), methodArgs);
-    } catch (IllegalAccessException ex) {
-        throw new TclRuntimeError("IllegalAccessException in execReflection");
-    } catch (IllegalArgumentException ex) {
-        throw new TclRuntimeError("IllegalArgumentException in execReflection");
-    } catch (InvocationTargetException ex) {
-        Throwable t = ex.getTargetException();
-
-        if (t instanceof Error) {
-            throw (Error) t;
-        } else if (t instanceof IOException) {
-            throw (IOException) t;
-        } else {
-           throw new TclRuntimeError("unexected exception in execReflection"); 
-        }
-    }
+    return Runtime.getRuntime().exec(strv, null, interp.getWorkingDir());
 }
 
 } // end ExecCmd
