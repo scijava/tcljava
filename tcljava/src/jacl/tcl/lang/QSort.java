b8ad52af59dcc0b4a50eb6e696ee1559f4823e60
mdejong <mdejong>
2006-07-19 00:06:09 +0000

    * src/jacl/tcl/lang/QSort.java (doDictionary):         Reimplement dictionary compare based on Tcl 8.4.         * tests/tcl/lsort.test: Add test cases that         expose broken disctionary sort order.

--- tcljava/src/jacl/tcl/lang/QSort.java
+++ tcljava/src/jacl/tcl/lang/QSort.java
@@ -8,7 +8,7 @@
  * redistribution of this file, and for a DISCLAIMER OF ALL
  * WARRANTIES.
  * 
- * RCS: @(#) $Id: QSort.java,v 1.2 1999/05/09 01:14:07 dejong Exp $
+ * RCS: @(#) $Id: QSort.java,v 1.2.12.1 2006/07/19 00:06:09 mdejong Exp $
  *
  */
 
@@ -295,6 +295,8 @@ final class QSort {
 
 
     /**
+     * DictionaryCompare -> doDictionary
+     *
      * Compares the order of two strings in "dictionary" order.
      *
      * @param str1 first item.
@@ -302,22 +304,17 @@ final class QSort {
      * @return 0 if they are equal, 1 if obj1 > obj2, -1 otherwise.
      */
     private final int doDictionary(String str1, String str2) {
+	final int len1 = str1.length();
+	final int len2 = str2.length();
+	char c1, c2;
+	int i1 = 0, i2 = 0;
+
 	int diff = 0, zeros;
 	int secondaryDiff = 0;
 
-	boolean cont = true;
-	int i1 = 0, i2 = 0;
-	int len1 = str1.length();
-	int len2 = str2.length();
-
-
-	while (cont) {
-	    if(i1 >= len1 || i2 >= len2) {
-		break;
-	    }
-
-	    if (Character.isDigit(str2.charAt(i2)) && 
-		    Character.isDigit(str1.charAt(i1))) {
+	while (true) {
+	    if (Character.isDigit(charAt(str2, i2, len2))
+		    && Character.isDigit(charAt(str1, i1, len1))) {
 
 		// There are decimal numbers embedded in the two
 		// strings.  Compare them as numbers, rather than
@@ -326,18 +323,19 @@ final class QSort {
 		// later, but only as a secondary choice.
 
 		zeros = 0;
-		while ((i2 < (len2 - 1)) && (str2.charAt(i2) == '0')) {
+		while (charAt(str2, i2, len2) == '0' &&
+                        Character.isDigit(charAt(str2, i2+1, len2))) {
 		    i2++;
 		    zeros--;
 		}
-		while ((i1 < (len1 - 1)) && (str1.charAt(i1) == '0')) {
+		while (charAt(str1, i1, len1) == '0' &&
+                        Character.isDigit(charAt(str1, i1+1, len1))) {
 		    i1++;
 		    zeros++;
 		}
 		if (secondaryDiff == 0) {
 		    secondaryDiff = zeros;
 		}
-
 
 		// The code below compares the numbers in the two
 		// strings without ever converting them to integers.  It
@@ -346,86 +344,76 @@ final class QSort {
 
 		diff = 0;
 		while (true) {
-
-		    if(i1 >= len1 || i2 >= len2) {
-		        cont = false;
-			break;
-		    }
 		    if (diff == 0) {
-			diff = str1.charAt(i1) - str2.charAt(i2);
+		        diff = charAt(str1, i1, len1) - charAt(str2, i2, len2);
 		    }
-		    i1++;
 		    i2++;
-		    if(i1 >= len1 || i2 >= len2) {
-		        cont = false;
-		        break;
-		    }
-
-
-		    if (!Character.isDigit(str2.charAt(i2))) {
-			if (Character.isDigit(str1.charAt(i1))) {
+		    i1++;
+		    if (!Character.isDigit(charAt(str2, i2, len2))) {
+		        if (Character.isDigit(charAt(str1, i1, len1))) {
 			    return 1;
-			} else {
+		        } else {
+			    // The two numbers have the same length. See
+			    // if their values are different.
+
 			    if (diff != 0) {
-				return diff;
+			        return diff;
 			    }
 			    break;
-			}
-		    } else if (!Character.isDigit(str1.charAt(i1))) {
-			return -1;
+		        }
+		    } else if (!Character.isDigit(charAt(str1, i1, len1))) {
+		        return -1;
 		    }
 		}
 		continue;
 	    }
-	    diff = str1.charAt(i1) - str2.charAt(i2);
+
+	    if (charAt(str1, i1, len1) != '\0' && charAt(str2, i2, len2) != '\0') {
+	        c1 = charAt(str1, i1, len1);
+	        i1++;
+	        c2 = charAt(str2, i2, len2);
+	        i2++;
+
+	        // Convert both chars to lower for the comparison, because
+	        // dictionary sorts are case insensitve.  Covert to lower, not
+	        // upper, so chars between Z and a will sort before A (where most
+	        // other interesting punctuations occur)
+
+	        c1 = Character.toLowerCase(c1);
+	        c2 = Character.toLowerCase(c2);
+	    } else {
+	        diff = (int) (charAt(str1, i1, len1) - charAt(str2, i2, len2));
+	        break;
+	    }
+
+	    diff = (int) (c1 - c2);
 	    if (diff != 0) {
-		if (Character.isUpperCase(str1.charAt(i1)) && 
-			Character.isLowerCase(str2.charAt(i2))) {
-		    diff = Character.toLowerCase(str1.charAt(i1)) - 
-			str2.charAt(i2);
-		    if (diff != 0) {
-			return diff;
-		    } else if (secondaryDiff == 0) {
-			secondaryDiff = -1;
-		    }
-		} else if (Character.isUpperCase(str2.charAt(i2)) && 
-			Character.isLowerCase(str1.charAt(i1))) {
-		    diff = str1.charAt(i1) - 
-			Character.toLowerCase(str2.charAt(i2));
-		    if (diff != 0) {
-			return diff;
-		    } else if (secondaryDiff == 0) {
-			secondaryDiff = 1;
-		    }
-		} else {
-		    return diff;
-		}
+	        return diff;
+	    } else if (secondaryDiff == 0) {
+	        if (Character.isUpperCase(c1) &&
+		        Character.isLowerCase(c2)) {
+		    secondaryDiff = -1;
+	        } else if (Character.isUpperCase(c2)
+		        && Character.isLowerCase(c1)) {
+		    secondaryDiff = 1;
+	        }
 	    }
-	    i1++;
-	    i2++;
-
 	}
-
-	if(i1 >= len1 && i2 < len2) {
-	    if (!Character.isDigit(str2.charAt(i2))) {
-	        return 1;
-	    } else {
-	        return -1;
-	    }
-
-	} else if(i2 >= len2 && i1 < len1) {
-	    if (!Character.isDigit(str1.charAt(i1))) {
-	        return -1;
-	    } else {
-	        return 1;
-	    }
-
-	}
-
 	if (diff == 0) {
 	    diff = secondaryDiff;
 	}
 	return diff;
+    }
+
+    // Like String.charAt() but returns '\0' if
+    // index is larger than len.
+
+    private final char charAt(
+        final String str,
+        final int index,
+        final int len)
+    {
+        return (index < len ? str.charAt(index) : '\0');
     }
 }
 
